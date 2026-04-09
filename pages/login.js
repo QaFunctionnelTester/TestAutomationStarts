9dac6f964346e916d0570b12bdce13014789114c
medbouzidi <mohamed@ehdaco.onmicrosoft.commohamed@ehdaco.onmicrosoft.com>
2026-04-09 17:30:14 +0200

    https://github.com/QaFunctionnelTester/Playwright_ObjectModelPage.git

--- pages/login.js
+++ pages/login.js
@@ -1,19 +1,21 @@
-exports.loginPage = class loginPage {
+exports.LoginPage = class LoginPage {
+
     constructor(page) {
+
         this.page = page
-
-        this.username_testbox = page.getByRole('textbox', { name: 'Username' })
-        this.password_testbox = page.getByRole('textbox', { name: 'Password' })
-        this.login_button = page.getByRole('button', { name: ' Login' })
-
+        this.username_textbox = page.getByLabel('Username')
+        this.password_textbox = page.getByLabel('Password')
+        this.login_button = page.getByRole('button', { name: 'Login' })
     }
-    async gotologinPage() {
-            await this.page.goto('https://the-internet.herokuapp.com/login');
-        }
-        // au lieu d'écrire chaque méthode (username, pass, login) toute seule on fait comme ça 
-    async login(username, Password) {
-        await this.username_testbox.fill(username)
-        await this.password_testbox.fill(Password)
+
+    async gotoLoginPage() {
+        await this.page.goto('https://the-internet.herokuapp.com/login');
+    }
+
+    async login(username, password) {
+        await this.username_textbox.fill(username)
+        await this.password_textbox.fill(password)
         await this.login_button.click()
     }
+
 }
--- tests/demo/login.spec.js
+++ tests/demo/login.spec.js
@@ -1,31 +1,17 @@
 import { test, expect } from '@playwright/test';
-import { loginpage } from '../../pages login'
+import { LoginPage } from '../../pages/login'
 
 test('test', async({ page }) => {
-    const login = new loginpage(page)
-    await login.gotoLoginPage()
-    await login.login('tomsmith', 'SuperSecretPassword!')
-    await page.goto('https://the-internet.herokuapp.com/login');
-    await page.getByRole('textbox', { name: 'Username' }).click();
-    await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
-    await page.getByRole('textbox', { name: 'Password' }).click();
-    await page.getByText('SuperSecretPassword!').click();
-    await page.getByText('SuperSecretPassword!').click();
-    await page.getByText('SuperSecretPassword!').click();
-    await page.getByText('SuperSecretPassword!').click();
-    await page.getByRole('heading', { name: 'This is where you can log' }).click();
-    await page.getByRole('textbox', { name: 'Password' }).click();
-    await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
-    await page.getByRole('textbox', { name: 'Password' }).fill('S');
-    await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
-    await page.getByRole('textbox', { name: 'Password' }).fill('Super');
-    await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
-    await page.getByRole('textbox', { name: 'Password' }).fill('SuperS');
-    await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
-    await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecret');
-    await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
-    await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretP');
-    await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
-    await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
-    await page.getByRole('button', { name: ' Login' }).click();
+
+    const Login = new LoginPage(page)
+
+    await Login.gotoLoginPage()
+    await Login.login('tomsmith', 'SuperSecretPassword!')
+
+    // await page.goto('https://the-internet.herokuapp.com/login');
+    // await page.getByLabel('Username').click();
+    // await page.getByLabel('Username').fill('tomsmith');
+    // await page.getByLabel('Password').click();
+    // await page.getByLabel('Password').fill('SuperSecretPassword!');
+    // await page.getByRole('button', { name: ' Login' }).click();
 });
